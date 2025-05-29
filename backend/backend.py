6af8e97779f55587f8ff4c0301c79b16f4c925dc
mabuyahya <mabuyahya@student.42amman.com>
2025-05-29 11:56:46 +0300

    fix when click on ask button move in rout

--- backend/backend.py
+++ backend/backend.py
@@ -14,9 +14,11 @@ import flask
 
 app = flask.Flask(__name__)
 
-@app.route('/ask', methods=['GET', 'POST'])
+@app.route('/ask', methods=['POST'])
 def ask():
-    return '<h1>Hello, World!</h1>'
+    data = flask.request.get_json()
+    question = data['question']
+    return flask.jsonify({'answer': f'You asked: {question}'})
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ backend/introduction.md
@@ -0,0 +1,3 @@
+* now after doing the front and send and understand that now we need a server, now is the time to build a one.
+* to build a server we will use a flask python lib which will build to use the server
+* after building the server now 
